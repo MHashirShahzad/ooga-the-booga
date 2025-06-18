f8e89495c3ec78997e7e8850985b96876350b406
Hashir Shahzad <hashir447079@gmail.com>
2025-06-18 10:25:11 +0500

    Poyo poyo :P

--- props/level_end.gd
+++ props/level_end.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var polygon_2d: Polygon2D = $Polygon2D
+@onready var polygon_2d_2: Polygon2D = $Polygon2D2
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	polygon_2d.rotation_degrees += delta * 40
+	polygon_2d_2.rotation_degrees -= delta * 40
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player2D:
+		print(body)
